d38f017490dd63bebc635cb5fe793bc56470e630
iamsanga <iamsanga7@gmail.com>
2020-04-19 12:37:40 +0530

    Updates

---
+++ precache-manifest.19e16bed77ae3278f6ce768bd8d7c2c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3d4a08d3bf6936730e3",
+    "url": "/test-app/static/js/main.f3d4a08d.chunk.js"
+  },
+  {
+    "revision": "685775ef98630cf8cf71",
+    "url": "/test-app/static/css/1.5db169f7.chunk.css"
+  },
+  {
+    "revision": "685775ef98630cf8cf71",
+    "url": "/test-app/static/js/1.685775ef.chunk.js"
+  },
+  {
+    "revision": "3510dee076921078b779",
+    "url": "/test-app/static/js/runtime~main.3510dee0.js"
+  },
+  {
+    "revision": "9a0078c09cf1247c985071e2abc26919",
+    "url": "/test-app/index.html"
+  }
+];
